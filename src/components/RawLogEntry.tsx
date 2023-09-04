a23a33ff0b96e6ceaf27e450c3be50433b21a939
Supalosa <supalosa@supalosa.com>
2023-09-05 00:06:59 +1000

    image, yay

--- src/components/RawLogEntry.tsx
+++ src/components/RawLogEntry.tsx
@@ -1,14 +1,24 @@
-import { Table } from "semantic-ui-react";
+import { Table, Image } from "semantic-ui-react";
 
 type RawLogEntryProps = {
     entry: any;
 }
 
+const renderExtra = (entry) => {
+    const { type, payload } = entry;
+    if (type === 'LogPlayerMoved') {
+        const { x, y, plane, regionId } = payload.newPosition;
+        return <Image src={`https://cdn-osrs-combat-logs.netlify.app/${plane}/region-${regionId}.png`} />;
+    }
+    return null;
+}
+
 export const RawLogEntry = (props: RawLogEntryProps) => {
     const { entry } = props;
+    const extra = renderExtra(entry);
     return <Table.Row>
         <Table.Cell>{entry.tick}</Table.Cell>
         <Table.Cell>{entry.type}</Table.Cell>
-        <Table.Cell>{JSON.stringify(entry.payload)}</Table.Cell>
+        <Table.Cell>{JSON.stringify(entry.payload)}{extra}</Table.Cell>
     </Table.Row>;
 }
